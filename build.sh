96628d426d9666cb687240a262d165215586f038
Mihai Andrei Spadaru <maspadaru@gmail.com>
2018-07-30 19:48:31 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
-rm -rf cmake-build
-mkdir cmake-build
-cd cmake-build
-cmake ..
-make
-cd ..
+
+PROJECT_NAME=LaserPP
+
+build_release () {
+    rm -rf cmake-build-release
+    mkdir cmake-build-release
+    cd cmake-build-release
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+    make
+    cd ..
+}
+
+build_debug () {
+    rm -rf cmake-build-debug
+    mkdir cmake-build-debug
+    cd cmake-build-debug
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    make
+    cd ..
+}
+
+clean_up () {
+    rm -rf cmake-build-debug
+    rm -rf cmake-build-release
+}
+
+run_project () {
+    cmake-build-debug/$PROJECT_NAME
+}
+
+if [ $1 = "b" ]; then
+    echo b
+    build_release
+fi
+if [ $1 = "d" ]; then
+    echo d
+    build_debug
+fi
+if [ $1 = "c" ]; then
+    echo c
+    clean_up
+fi
+if [ $1 = "r" ]; then
+    echo r
+    run_project
+fi
+
+
