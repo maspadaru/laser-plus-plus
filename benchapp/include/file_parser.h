26cb897c4550b463947c931302fffc91d5c528b7
Mihai-Andrei Spadaru <maspadaru@gmail.com>
2018-11-28 17:09:20 +0100

    simplified Benchapp parser

--- benchapp/include/file_parser.h
+++ benchapp/include/file_parser.h
@@ -5,71 +5,25 @@
 #ifndef BENCHAPP_FILE_PARSER_H
 #define BENCHAPP_FILE_PARSER_H
 
-
-#include <tuple>
-#include <vector>
 #include <algorithm>
-#include <stack>
 #include <sstream>
-#include <utility>
+#include <stack>
 #include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
-#include <util/format_exception.h>
 #include <util/data_atom.h>
-
-enum class TokenType {
-    OPERATOR,
-    OPEN_PARENTHESES,
-    CLOSED_PARENTHESES,
-    IDENTIFIER
-};
-
-struct Token {
-    TokenType type;
-    std::string value;
-};
-
-
-
-// Helper functions:
-
-static inline void rtrim(std::string *s);
-
-static inline void ltrim(std::string *s);
-
-static inline void trim(std::string *s);
-
-static inline void syntax_error(std::string error_message);
-
+#include <util/format_exception.h>
 class FileParser {
-private:
+  private:
+    laser::util::DataAtom parse_string(std::string const &input);
 
-    std::vector<laser::util::DataAtom> argument_stack;
-
-    Token recognize(std::string token_string) const;
-
-    std::vector<Token> tokenize(std::string const &rule_string) const;
-
-    std::vector<Token> add_token_attempt(
-            std::vector<Token> token_vector,
-            std::ostringstream &token_stream
-    ) const;
-
-    std::vector<Token> add_new_token(
-            std::vector<Token> token_vector,
-            TokenType type, char value_char) const;
-
-    std::vector<laser::util::DataAtom>
-    parse_token_vector(std::vector<Token> input_token_vector);
-
-public:
-
+  public:
     ~FileParser() = default;
 
     std::vector<laser::util::DataAtom>
     parse_data(std::vector<std::string> raw_data_vector);
-
 };
-
 
 #endif // BENCHAPP_FILE_PARSER_H
--- benchapp/src/file_parser.cpp
+++ benchapp/src/file_parser.cpp
@@ -4,156 +4,27 @@
 
 #include "file_parser.h"
 
-/* trim from start (in place)
- * source:
- * https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
- */
-static inline void ltrim(std::string *s) {
-    s->erase(s->begin(), std::find_if(s->begin(), s->end(), [](int ch) {
-        return !std::isspace(ch);
-    }));
-}
-
-/* trim from end (in place)
- * source:
- * https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
- */
-static inline void rtrim(std::string *s) {
-    s->erase(std::find_if(s->rbegin(), s->rend(), [](int ch) {
-        return !std::isspace(ch);
-    }).base(), s->end());
-}
-
-/* trim from both ends (in place)
- * source:
- * https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
- */
-static inline void trim(std::string *s) {
-    ltrim(s);
-    rtrim(s);
-}
-
-static inline void syntax_error(std::string error_message) {
-    std::string message = "Syntax Error in File Parser: " + error_message;
-    const char *exception_message = message.c_str();
-    throw laser::util::FormatException(exception_message);
-
-}
-
-// === Tokenizer ===
-
-Token FileParser::recognize(std::string token_string) const {
-    trim(&token_string);
-    Token token;
-    token.value = token_string;
-    if (token_string == ",") {
-        token.type = TokenType::OPERATOR;
-    } else {
-        token.type = TokenType::IDENTIFIER;
-    }
-    return token;
-}
-
-std::vector<Token> FileParser::add_token_attempt(
-        std::vector<Token> token_vector,
-        std::ostringstream &token_stream) const {
-    std::string str = token_stream.str();
-    if (!str.empty()) {
-        Token token = recognize(str);
-        token_vector.push_back(token);
-    }
-    token_stream.clear();
-    token_stream.str("");
-    return token_vector;
-}
-
-std::vector<Token> FileParser::add_new_token(
-        std::vector<Token> token_vector,
-        TokenType type, char value_char) const {
-    Token token;
-    token.type = type;
-    std::string value_string(1, value_char);
-    token.value = value_string;
-    token_vector.push_back(token);
-    return token_vector;
-}
-
-std::vector<Token> FileParser::tokenize(std::string const &rule_string) const {
-    std::vector<Token> token_vector;
-    std::istringstream rule_stream(rule_string);
-    std::ostringstream token_stream;
-    char c;
-    while (rule_stream >> c) {
-        if (c == ',') {
-            token_vector = add_token_attempt(token_vector, token_stream);
-            token_vector = add_new_token(token_vector, TokenType::OPERATOR,
-                    c);
-
-        } else if (c == '(') {
-            token_vector = add_token_attempt(token_vector, token_stream);
-            token_vector =
-                    add_new_token(token_vector, TokenType::OPEN_PARENTHESES, c);
-        } else if (c == ')') {
-            token_vector = add_token_attempt(token_vector, token_stream);
-            token_vector =
-                    add_new_token(token_vector, TokenType::CLOSED_PARENTHESES,
-                            c);
-        } else {
-            token_stream << c;
-        }
-        int stream_peek = rule_stream.peek();
-        if (stream_peek == ' ') {
-            token_vector = add_token_attempt(token_vector, token_stream);
-        }
-    }
-    token_vector = add_token_attempt(token_vector, token_stream);
-    return token_vector;
-}
-
-// ======== Parser =========
-
-
-std::vector<laser::util::DataAtom>
-FileParser::parse_token_vector(
-        std::vector<Token> input_token_vector) {
+laser::util::DataAtom FileParser::parse_string(std::string const &input) {
     std::vector<std::string> token_list;
-    std::string predicate = input_token_vector[0].value;
-    std::vector<laser::util::DataAtom> result;
-    for (int index = 1; index < input_token_vector.size(); index++) {
-        Token token = input_token_vector[index];
-        switch (token.type) {
-            case TokenType::OPERATOR : {
-                // TODO: ignore for now, use later for syntax check
-                break;
-            }
-            case TokenType::IDENTIFIER : {
-                token_list.push_back(token.value);
-                break;
-            }
-        }
+    std::string token;
+    std::string predicate;
+    std::stringstream iss(input);
+    iss >> predicate;
+    while (iss >> token) {
+        token_list.push_back(token);
     }
-    result.emplace_back(predicate, token_list);
-    token_list.clear();
+    laser::util::DataAtom result(predicate, token_list);
     return result;
 }
 
 std::vector<laser::util::DataAtom>
-FileParser::parse_data(
-        std::vector<std::string>
-        raw_data_vector) {
-    std::vector<laser::util::DataAtom> data_atom_vector;
+FileParser::parse_data(std::vector<std::string> raw_data_vector) {
+    std::vector<laser::util::DataAtom> result;
     for (const auto &raw_string : raw_data_vector) {
-        auto token_vector = tokenize(raw_string);
-        if (!token_vector.empty()) {
-            auto temp_vector =
-                    parse_token_vector(std::move(token_vector));
-            data_atom_vector
-                    .insert(data_atom_vector.end(), temp_vector.begin(),
-                            temp_vector.end());
+        if (raw_string.size() > 0) {
+            auto fact = parse_string(raw_string);
+            result.push_back(fact);
         }
     }
-    return data_atom_vector;
+    return result;
 }
-
-
-
