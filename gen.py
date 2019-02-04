5c097b1884855967c55fac70e458cd26ad5b0e94
Mike Spadaru <maspadaru@gmail.com>
2019-02-04 16:37:50 +0100

    Added readme

--- gen.py
+++ gen.py
@@ -1,7 +1,21 @@
+'''
+This script generates some random tuples that can be used for benchmarking. 
+The total number of tuples generated can be set by modifying NUMBER_OF_TUPLES.
+Each tuple has a random predicate from the set: ('a', 'b', 'c', 'd', 'e', 'f').
+Each tuple has a number of variables determined by its predicate. 
+The generated tuples are printed to stdin, separated by a new line character.
+In order to generate a file useful for benchmarking, redirect this output to 
+a file.
+Example use:
+    python gen.py > input.txt
+'''
+
 import random
 import string
 import sys
 import os
+
+NUMBER_OF_TUPLES = 100000009
 
 def gen_const():
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2))
@@ -9,7 +23,7 @@ def gen_const():
 def main():
     random.seed()
 
-    for i in range(1,100000009):
+    for i in range(1,NUMBER_OF_TUPLES):
         pred = random.randint(1, 6)
         if (pred == 1):
             print("%s %s %s %s" % ('a', gen_const(), gen_const(), gen_const() ))
