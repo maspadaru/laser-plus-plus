03c37d714d1038e1834a2c38d36f9746333ac91f
Mike Spadaru <maspadaru@tutanota.com>
2019-08-16 16:02:39 +0200

    some work on math atoms - fix

--- src/rule/math_atom.cpp
+++ src/rule/math_atom.cpp
@@ -5,17 +5,17 @@ namespace laser::rule {
 std::vector<std::shared_ptr<util::Grounding>>
 MathAtom::evaluate(std::vector<std::shared_ptr<util::Grounding>> body_facts) {
 
-    auto left_index = body.get_variable_index(left_name);
-    if (left_index > 0) {
-        for (auto &body_fact: body_facts) {
-            auto body_constant = body_fact.get_constant(left_index);
-            int64_t left_value = strtoll(body_constant, NULL, 0); 
-            int64_t result = left_value + right_value;
-            //add variable to groundig on the positon of rule::trigger_variables
+    //auto left_index = body.get_variable_index(left_name);
+    //if (left_index > 0) {
+        //for (auto &body_fact: body_facts) {
+            //auto body_constant = body_fact.get_constant(left_index);
+            //int64_t left_value = strtoll(body_constant, NULL, 0); 
+            //int64_t result = left_value + right_value;
+            ////add variable to groundig on the positon of rule::trigger_variables
 
-        }
+        //}
 
-    }
+    //}
 
 
     return body_facts;
